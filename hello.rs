1f9f364e3664c74fc49d0e21ee3a9ba8f690e86d
Hans-Peter Klett <hans@hansdude.com>
2016-03-05 16:30:17 -0700

    getting there

--- hello.rs
+++ hello.rs
@@ -1,5 +1,6 @@
 use std::f64;
 use std::iter;
+use std::mem;
 
 struct Neuron {
     weights: Vec<f64>,
@@ -13,7 +14,25 @@ impl Neuron {
         Neuron { weights: weights, new_weights: vec![], output: 0.0, delta: 0.0 }
     }
 
-    fn compute_new_weights(&self, learningRate : f64) {
+    fn compute_delta(&mut self, errorFromOutput: f64) {
+        self.delta = errorFromOutput * self.output * (1.0 * self.output)
+    }
+
+    fn compute_new_weights(&mut self, learningRate : f64, inputs: Vec<f64>) {
+        let delta = self.delta;
+        self.new_weights = iter::once(1.0).chain(inputs)
+            .zip(self.weights.to_owned())
+            .map(|(weight, input)| weight + learningRate * delta * input)
+            .collect();
+    }
+
+    fn use_new_weights(&mut self) {
+        self.weights = mem::replace(&mut self.new_weights, vec![]);
+    }
+
+    fn error_from_inputs(&self) -> Vec<f64> {
+        let delta = self.delta;
+        self.weights.iter().skip(1).map(|weight| weight * delta).collect()
     }
 }
 
@@ -30,6 +49,11 @@ impl Layer {
     fn outputs(&self) -> Vec<f64> {
         self.neurons.iter().map(|neuron| neuron.output).collect()
     }
+
+    fn error_from_inputs(&self) -> Vec<f64> {
+        let errors : Vec<Vec<f64>> = self.neurons.iter().map(|neuron| neuron.error_from_inputs()).collect();
+        (0..errors[0].len()).map(|i| errors.iter().fold(0.0, |acc, err| acc + err[i])).collect()
+    }
 }
 
 struct Entry {
@@ -40,9 +64,6 @@ struct Entry {
 fn sigmoid(x: f64) -> f64 {
     let c = 1.0;
     1.0 / (1.0 + f64::exp(-c * x))
-}
-fn _sigmoid_prime(x: f64) -> f64 {
-    sigmoid(x) * (1.0 - sigmoid(x))
 }
 
 fn main() {
@@ -82,6 +103,24 @@ fn main() {
     }
 
     println!("output");
+}
+
+fn far(learningRate : f64, entry: &Entry, layers: &mut Vec<Layer>) {
+    let mut errorFromOutput : Vec<f64> =
+        (&layers[0]).outputs().iter().zip(&entry.outputs)
+            .map(|(out, target)| out - target)
+            .collect();
+
+    //(&mut layers[0]).neurons[0].compute_delta(0.0);
+    //for layer in layers.iter().rev() {
+        //for (neuron, efo) in (&mut layer.neurons).iter().zip(&errorFromOutput) {
+            //neuron.compute_delta(*efo);
+        //}
+    //}
+
+    // compute_delta for each neuron in final layer
+    // compute_new_weights for each neuron in final layer
+    // calculate errorFromOutput for ea
 }
 
 fn backward(learningRate : f64, entry: &Entry, layers: &mut Vec<Layer>) {
